24ba137861476e4c9ca0e7dc52338f0d6778725d
Everton Paixão <evertonpaixao@gmail.com>
2025-04-02 15:39:35 -0300

    login admin 2

--- api/admin/login_admin.php
+++ api/admin/login_admin.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Login do Admin</title>
+    <title>Login do Admin 2</title>
 </head>
 <body>
-    <h1>Login do Admin</h1>
+    <h1>Login do Admin 2</h1>
     <form action="auth_admin.php" method="post">
         Usuário: <input type="text" name="usuario" required><br>
         Senha: <input type="password" name="senha" required><br>
