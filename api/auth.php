e7dbf94734f1e0e1ca169da690f4256d65ea07d3
Everton Paixão <evertonpaixao@gmail.com>
2025-04-02 16:49:19 -0300

    auth com modificação do cookies e add header

--- api/auth.php
+++ api/auth.php
@@ -8,8 +8,11 @@ $nome = $_POST['nome'] ?? '';
 $senha = $_POST['senha'] ?? '';
 
 if (isset($motoristas[$nome]) && $motoristas[$nome] === $senha) {
+
+    header('Set-Cookie: motorista_logado=' . $nome . '; Path=/; Secure; HttpOnly; SameSite=None');
+
     // Criar o cookie
-    setcookie('motorista_logado', $nome, time() + 3600, '/', '', false, true);
+    setcookie('motorista_logado', $nome, time() + 3600, '/', '', true, true);
 
     // Redireciona com um parâmetro para indicar sucesso
     header('Location: formulario.php?login=sucesso');
